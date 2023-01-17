d8f6880ea1586cf59b651bce5bbaed07d6d73cc1
MO ZHAO <morrisz@student.ubc.ca>
2023-01-17 15:57:00 -0800

    add structure of docstring

---
+++ R/visualizations.R
@@ -0,0 +1,16 @@
+# Author: Morris Zhao
+# Date: 2023-01-17
+
+#' Title
+#'
+#' @param model
+#' @param price
+#' @param market_df
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot__mercedes_price <- function(model, price, market_df) {
+
+}
