4678907891d3b4e66640fc13cd73071f46edf5cb
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-01-21 00:26:26 -0800

    minor edit

--- R/predict.R
+++ R/predict.R
@@ -4,15 +4,15 @@
 
 #' Predicts the price in USD of a Mercedes-Benz.
 #'
-#' @param model the model of the mercedes
-#' @param year the year the car was made
-#' @param odometer_mi the odomoteter reading of the vehicle
+#' @param model the model of the mercedes, character
+#' @param year the year the car was made, integer
+#' @param odometer_mi the odomoteter reading of the vehicle, integer
 #' @param condition the physical condition of the vehicle, must be one
-#'    of 'new', 'like new', 'excellent', 'good', 'fair', 'salvage'
-#' @param paint_color the color the car is painted
-#' @param version the model version to use, format 'vX'
+#'    of 'new', 'like new', 'excellent', 'good', 'fair', 'salvage', character
+#' @param paint_color the color the car is painted, character
+#' @param version the model version to use, format 'vX', character
 #'
-#' @return the price of the car as a float
+#' @return the price of the car as a numeric
 #' @export
 #'
 #' @examples
