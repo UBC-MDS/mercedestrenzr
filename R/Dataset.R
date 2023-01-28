0f5e9769e462b31303dc2e081a271374597b11c5
kellywujy <kellywu817@outlook.com>
2023-01-28 12:32:13 -0800

    Dataset documentation added

---
+++ R/Dataset.R
@@ -0,0 +1,30 @@
+#' Mercedes-Benz Used-car Dataset
+#'
+#' A subset of data from the Used-car data
+#'
+#' @format ## `mercedes_data`
+#' A data frame with 5,553 rows and 17 columns:
+#' \describe{
+#'   \item{price_USD}{Price of car in US dollar}
+#'   \item{condition}{Condition of the car}
+#'   \item{paint_color}{paint color}
+#'   \item{model}{model of the car}
+#'   \item{odometer_mi}{odometer (in miles) of the car}
+#'   \item{year}{year of production}
+#'   \item{num_cylinders}{number of cylinders of the car}
+#'   \item{fuel}{fuel needed of the car}
+#'   \item{transmission}{transmission of the car}
+#'   \item{drive}{drive of the car}
+#'   \item{size}{size of the car}
+#'   \item{type}{type of the car}
+#'   \item{state}{location (state) of the car}
+#'   \item{VIN}{VIN number of the car}
+#'   \item{title_status}{title status of the car}
+#'   \item{description}{description of the car}
+#'
+#'
+
+#'
+#' }
+#' @source <https://github.com/AustinReese/UsedVehicleSearch>
+"mercedes_data"
