9f59ad78c227db2a3a5f318989e6fcedafad0dc9
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-01-28 19:28:22 -0800

    doc: added basic model docs

--- data/mercedes_price_model.R
+++ data/mercedes_price_model.R
@@ -4,5 +4,11 @@
 library(here)
 library(bundle)
 
+#' A pretrained model to predict mercedes prices
+#'
+#' An XGboost model trained and tuned on the sample mercedes dataset.
+#'
+#' @format A tidymodels workflow including XGboost model.
+#' @export
 mercedes_price_model <- readRDS(here::here("data", "price-prediction-model_v1.rds")) |>
   bundle::unbundle()
