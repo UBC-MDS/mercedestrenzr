ec8dc705b7897115644edae8b705fd3119c6d519
Ty Andrews <ty.elgin.andrews@gmail.com>
2023-01-28 19:03:23 -0800

    feat: predict_mercedes_price function completed

--- R/predict.R
+++ R/predict.R
@@ -13,10 +13,58 @@
 #' @param version the model version to use, format 'vX', character
 #'
 #' @return the price of the car as a numeric
+#' @import tidyverse
+#' @import tidymodels
+#' @import here
+#' @import bundle
+#' @import rlang
 #' @export
 #'
 #' @examples
 #' price <- predict_mercedes_price("e-class", 2015, 55000, "fair", "silver")
 predict_mercedes_price <- function(model, year, odometer_mi, condition, paint_color, version="v1") {
+
+  if(!is.character(model)){
+    stop("The first input should be a character contain the specific model")
+  }
+
+  if(!is.numeric(year)){
+    stop("The second input should be the year of the car as a whole number, e.g. 1979")
+  }
+
+  if(!is.numeric(odometer_mi)){
+    stop("The third input should be the miles on the odometer as whole number.")
+  }
+
+  if(odometer_mi < 0){
+    stop("The odometer_mi must be greater than 0")
+  }
+
+  if(!is.character(condition)){
+    stop("The forth input should be the condition of the car as characters, one of salvage, used, fair, good, excellent, like new, new")
+  }
+
+  if(!is.element(condition, c('salvage', 'used', 'fair', 'good', 'excellent', 'like new', 'new'))){
+    stop("The forth input should be one of salvage, used, fair, good, excellent, like new, new")
+  }
+
+  if(!is.character(paint_color)){
+    stop("The fifth input should be the color of the car as a character, e.g. 'red'")
+  }
+
+  new_data <- data.frame(year=c(year),
+                         condition=c(condition),
+                         model=c(model),
+                         paint_color=c(paint_color),
+                         odometer_mi = c(odometer_mi),
+                         stringsAsFactors=TRUE
+  ) |>
+    mutate(
+      condition = factor(condition, ordered = TRUE, levels = c("salvage", "used", "fair", "good", "excellent", "like new", "new"))
+    )
+
+  predict(mercedes_price_model,
+          new_data = new_data) |>
+    rename(predicted_price_USD = .pred)
 
 }
